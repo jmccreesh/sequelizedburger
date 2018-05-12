95d17841cb0c7c49653279cee9399a8de3850ef2
Joe McCreesh <jmccreesh@libertyintegrated.com>
2018-05-11 21:26:14 -0400

    changes

---
+++ SequelizedBurger.md
@@ -0,0 +1,42 @@
+﻿# Eat-Da-Burgers
+*A a restaurant app that lets users input the names of burgers they'd like to eat..* *Try it HERE!===>* [Friend-Finder](https://fast-scrubland-53335.herokuapp.com)
+
+----------
+
+## Overview
+
+Friend Finder is a full stack site that will allow a user to answer 10 basic compatibility questions. The results of this survey
+will then be compared to other users answers to the same survey questions. Upon computing the differences between users responses
+and comparing them to each other, the application will then display the name and the picture of the user with the best overall match.
+
+## Figure 1. The Home Page
+
+- **This image depicts the home page before the survey is completed**
+
+![image1](app/assets/images/image1.png)
+
+## Figure 2. The Survey
+
+- **This image depicts the survey and user inputs**
+
+![image2](app/assets/images/image2.png)
+
+## Figure 3. The results
+
+- **This image depicts the best match for the user**
+
+![image3](app/assets/images/image3.png)
+
+## Technology used on this project
+
+- **JavaScript**, **J-Query**
+- **HTML**, **CSS**,**Bootstrap**
+- **Node.js**, **Express-Node Package**, **Body-Parser-Node Package**, **Path-Node Package**
+- **GitHub**
+- **Heroku**
+
+----------
+
+## Author
+
+**Joe McCreesh**
---
+++ controllers/burgersController.js
@@ -0,0 +1,121 @@
+
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our models
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // GET route for getting all of the todos
+  app.get("/", function(req, res) {
+    // findAll returns all entries for a table when used with no options
+    console.log(db.Burgers);
+    db.Burgers.findAll({}).then(function(dbBurgers) {
+      // We have access to the todos as an argument inside of the callback function
+     //Use Render
+     console.log({dbBurgers})
+     res.render("index", {'burger_data': dbBurgers} );
+      // res.json(dbBurgers);
+    });
+  });
+  // POST route for saving a new todo
+  app.post("/burgers/create", function(req, res) {
+    console.log(req.body);
+    // create takes an argument of an object describing the item we want to
+    // insert into our table. In this case we just we pass in an object with a text
+    // and complete property
+    db.Burgers.create({
+      //burger_name, devoured
+      burger_name: req.body.burger_name,
+      devoured: req.body.devoured
+    }).then(function(dbBurgers) {
+      // We have access to the new todo as an argument inside of the callback function
+      // res.json(dbBurgers);
+       res.redirect("/");
+    });
+  });
+
+  // DELETE route for deleting todos. We can get the id of the todo to be deleted from
+  // req.params.id
+  app.delete("/burgers/:id", function(req, res) {
+    // We just have to specify which todo we want to destroy with "where"
+    db.Burgers.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbBurgers) {
+      res.json(dbBurgers);
+      res.redirect("/");
+    });
+
+  });
+
+  // PUT route for updating todos. We can get the updated todo data from req.body
+  app.put("/burgers/update", function(req, res) {
+    // Update takes in an object describing the properties we want to update, and
+    // we use where to describe which objects we want to update
+    //burger_name, devoured
+    db.Burgers.update({
+      devoured: true
+    }, {
+      where: {
+        id: req.body.burger_id
+      }
+    }).then(function(dbBurgers) {
+      // res.json(dbBurgers);
+       res.redirect("/");
+
+    });
+  });
+
+};
+
+
+
+// var express = require("express");
+
+// var router = express.Router();
+// var burgers = require("../models/burger.js");
+
+// // get route -> index
+// router.get("/", function(req, res) {
+//   res.redirect("/burgers");
+// });
+
+// router.get("/burgers", function(req, res) {
+//   // express callback response by calling burger.selectAllBurger
+//   burgers.all(function(burgerData) {
+//     // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
+//     res.render("index", { burger_data: burgerData });
+//   });
+// });
+
+// // post route -> back to index
+// router.post("/burgers/create", function(req, res) {
+//   // takes the request object using it as input for buger.addBurger
+//   burgers.create(req.body.burger_name, function(result) {
+//     // wrapper for orm.js that using MySQL insert callback will return a log to console,
+//     // render back to index with handle
+//     console.log(result);
+//     res.redirect("/");
+//   });
+// });
+
+// // put route -> back to index
+// router.put("/burgers/update", function(req, res) {
+//   burgers.update(req.body.burger_id, function(result) {
+//     // wrapper for orm.js that using MySQL update callback will return a log to console,
+//     // render back to index with handle
+//     console.log(result);
+//     res.redirect("/");
+//   });
+// });
+
+// module.exports = router;
